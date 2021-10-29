a9eceaeef70e690bee19aeee24f8c5341e51cc2a
Janek Rahrt <janek.rahrt@me.com>
2021-10-29 17:48:37 +0200

    feat: update hash_state
    Using this implementation you'd save one hash round every time this function is called. Also it makes it easier to implement recursively in other languages.

--- src/starkware/cairo/common/hash_state.py
+++ src/starkware/cairo/common/hash_state.py
@@ -6,10 +6,10 @@ from starkware.cairo.lang.vm.crypto import pedersen_hash
 def compute_hash_on_elements(data, hash_func=pedersen_hash):
     """
     Computes a hash chain over the data, in the following order:
-        h(h(h(h(0, data[0]), data[1]), ...), data[n-1]), n).
+        h(h(h(n, data[0]), data[1]), ...), data[n-1]).
 
-    The hash is initialized with 0 and ends with the data length appended.
-    The length is appended in order to avoid collisions of the following kind:
+    The hash is initialized with the data length.
+    The length is used in order to avoid collisions of the following kind:
     H([x,y,z]) = h(h(x,y),z) = H([w, z]) where w = h(x,y).
     """
-    return functools.reduce(lambda x, y: hash_func(x, y), [*data, len(data)], 0)
+    return functools.reduce(lambda x, y: hash_func(x, y), [*data], len(data))
