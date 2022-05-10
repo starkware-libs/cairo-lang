2abd303e1808612b724bc1412b2b5babd04bb4e7
Lior Goldberg <lior@starkware.co>
2022-05-10 18:01:27 +0300

    Cairo v0.8.2.1. (bug fix)

--- src/starkware/cairo/lang/setup.py
+++ src/starkware/cairo/lang/setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
     setup_requires=["wheel"],
     url="https://cairo-lang.org/",
     package_data={
-        "starkware.cairo.common": ["*.cairo"],
+        "starkware.cairo.common": ["*.cairo", "*/*.cairo"],
         "starkware.cairo.lang.compiler": ["cairo.ebnf", "lib/*.cairo"],
         "starkware.cairo.lang.tracer": ["*.html", "*.css", "*.js", "*.png"],
         "starkware.cairo.lang": ["VERSION"],
