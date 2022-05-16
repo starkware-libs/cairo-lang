fbe789d6ee8f3051adf5ab2bdb77707d0003f34e
Juan-M-V <juan.viel@lambdaclass.com>
2022-05-16 15:52:18 -0300

    Change the name of the disassembler

---
+++ disassembler.py
@@ -0,0 +1,39 @@
+import pprint as pp
+import sys
+# TODO See if its worth it to add __init__.py files to create a package
+sys.path.insert(1, './src/starkware/cairo/lang')
+
+from compiler import encode, instruction
+from vm import cairo_runner
+
+PRIME = 2 ** 251 + 17 * 2 ** 192 + 1
+
+def printdict(code):
+    runner=cairo_runner.get_runner_from_code(code, layout='plain', prime=PRIME)
+    mem = dict(runner.memory)
+    print(code)
+    pp.pprint({k:(hex(v) if type(v) == type(int()) else v) for k,v in mem.items()})
+
+    print("{")
+    it = iter(mem.items())
+    for keyval in it:
+        print(keyval[0], '-> ', end='')
+        if type(keyval[1]) != type(int()):
+            print('[', keyval[1], ']')
+        elif keyval[0].segment_index == 1:
+            print(hex(keyval[1]))
+        elif type(keyval[1]) == type(int()):
+            try:
+                pp.pprint(vars(encode.decode_instruction(keyval[1], None)))
+            except AssertionError:
+                pp.pprint(vars(encode.decode_instruction(keyval[1], next(it)[1])))
+
+def main():
+    filename = sys.argv[1]
+    with open(filename) as f:
+        code = f.read()
+    printdict(code)
+
+if __name__ == '__main__':
+    main()
+
